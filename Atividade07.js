70369cbe7f67518debfbcb825ef7b15a7a403604
Evelyn Oliveira <evelynoliveira.tds1.senai@gmail.com>
2024-08-08 11:50:14 -0300

    feat: exercices for review

---
+++ Atividade07.js
@@ -0,0 +1,9 @@
+let livrosLidos = ["É assim que acaba", "Tarde Demais", "Uma segunda chance", "É assim que começa", "Até o verão terminar", "Verity", "A biblioteca da meia noite"];
+let metaLivros = 10;
+let output = livrosLidos.join()
+
+for(let i = 0; i < livrosLidos; i++){
+    if(livrosLidos.length <= metaLivros [i]){
+        console.log("Meta Atingida: Parabéns! Você atingiu sua meta de leitura!")
+    }
+}
